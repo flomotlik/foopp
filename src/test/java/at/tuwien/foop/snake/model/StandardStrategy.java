f457481d11e2b3a4527f8e4063d665dc44e37598
Florian Motlik <flomotlik@gmail.com>
2010-05-15 13:07:14 +0200

    Standard Strategy implemented via Maps, not via custom methods
    Signed-off-by: Florian Motlik <flomotlik@gmail.com>

--- src/test/java/at/tuwien/foop/snake/model/StandardStrategy.java
+++ src/test/java/at/tuwien/foop/snake/model/StandardStrategy.java
@@ -1,32 +1,37 @@
 package at.tuwien.foop.snake.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import at.tuwien.foop.snake.interfaces.Colour;
 import at.tuwien.foop.snake.interfaces.Strategy;
 
 public class StandardStrategy implements Strategy {
+
+    private Map<Colour, Map<Colour, Colour>> strategies = new HashMap<Colour, Map<Colour, Colour>>();
+
+    public StandardStrategy() {
+        final Map<Colour, Colour> redStrategy = new HashMap<Colour, Colour>();
+        redStrategy.put(Colour.BLUE, Colour.RED);
+        redStrategy.put(Colour.GREEN, Colour.GREEN);
+        strategies.put(Colour.RED, redStrategy);
+
+        final Map<Colour, Colour> blueStrategy = new HashMap<Colour, Colour>();
+        blueStrategy.put(Colour.GREEN, Colour.BLUE);
+        blueStrategy.put(Colour.RED, Colour.RED);
+        strategies.put(Colour.BLUE, blueStrategy);
+
+        final Map<Colour, Colour> greenStrategy = new HashMap<Colour, Colour>();
+        greenStrategy.put(Colour.RED, Colour.GREEN);
+        greenStrategy.put(Colour.BLUE, Colour.BLUE);
+        strategies.put(Colour.GREEN, greenStrategy);
+    }
 
     @Override
     public Colour decideOnColour(Colour currentHead, Colour ingestedElement) {
         if (currentHead == ingestedElement) {
             return currentHead;
         }
-
-        switch (currentHead) {
-            case RED:
-                return redStrategy(ingestedElement);
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
-
-    private Colour redStrategy(Colour ingestedelement) {
-        switch (ingestedelement) {
-            case BLUE:
-                return Colour.RED;
-            case GREEN:
-                return Colour.GREEN;
-            default:
-                throw new IllegalArgumentException();
-        }
+        return this.strategies.get(currentHead).get(ingestedElement);
     }
 }
